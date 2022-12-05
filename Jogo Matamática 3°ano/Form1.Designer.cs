3ef0d514646153a74d8427923d6267222eeb0a5c
And5reas <and5reas.porcel@hotmail.com>
2022-12-05 11:48:35 -0300

    Foi feito algumas alterações na propriedade "Anchor". Para quando o usuário mudar para 'fullscreen' os elementos visuais fiquem organizados

--- Jogo Matamática 3°ano/Form1.Designer.cs
+++ Jogo Matamática 3°ano/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Jogo_Matamática_3_ano
 {
-    partial class Form1
+    partial class FrmJogo
     {
         /// <summary>
         /// Required designer variable.
@@ -33,21 +33,27 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(203, 77);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(266, 358);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(268, 80);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Jogar";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // FrmJogo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "FrmJogo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo Matemática 3°ano";
+            this.Load += new System.EventHandler(this.FrmJogo_Load);
             this.ResumeLayout(false);
 
         }
--- Jogo Matamática 3°ano/Program.cs
+++ Jogo Matamática 3°ano/Program.cs
@@ -16,7 +16,7 @@ namespace Jogo_Matamática_3_ano
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmJogo());
         }
     }
 }
