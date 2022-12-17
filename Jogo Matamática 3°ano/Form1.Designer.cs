a57e8fff02a8120a22e4a40b2fdfcbfa8cf1f16e
And5reas <and5reas.porcel@hotmail.com>
2022-12-17 18:50:21 -0300

    Implemented a DebugMode and the character moviment.

--- Jogo Matamática 3°ano/Form1.Designer.cs
+++ Jogo Matamática 3°ano/Form1.Designer.cs
@@ -28,63 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmJogo));
-            this.PnlFase1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.PnlMenu = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.PnlFase2 = new System.Windows.Forms.Panel();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.PnlFase1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.PbxFases = new System.Windows.Forms.PictureBox();
+            this.PbxPersonagem = new System.Windows.Forms.PictureBox();
+            this.TmrAndar = new System.Windows.Forms.Timer(this.components);
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.LblX = new System.Windows.Forms.Label();
+            this.LblY = new System.Windows.Forms.Label();
             this.PnlMenu.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            this.PnlFase2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PbxFases)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PbxPersonagem)).BeginInit();
             this.SuspendLayout();
-            // 
-            // PnlFase1
-            // 
-            this.PnlFase1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.PnlFase1.BackColor = System.Drawing.Color.IndianRed;
-            this.PnlFase1.Controls.Add(this.PnlFase2);
-            this.PnlFase1.Controls.Add(this.pictureBox2);
-            this.PnlFase1.Controls.Add(this.pictureBox1);
-            this.PnlFase1.Controls.Add(this.label2);
-            this.PnlFase1.Location = new System.Drawing.Point(0, 0);
-            this.PnlFase1.Name = "PnlFase1";
-            this.PnlFase1.Size = new System.Drawing.Size(1123, 696);
-            this.PnlFase1.TabIndex = 0;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(182, 73);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(758, 550);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(3, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(171, 55);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Fase 1";
             // 
             // PnlMenu
             // 
@@ -93,7 +51,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.PnlMenu.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.PnlMenu.Controls.Add(this.label1);
-            this.PnlMenu.Location = new System.Drawing.Point(0, 0);
+            this.PnlMenu.Location = new System.Drawing.Point(990, 573);
             this.PnlMenu.Name = "PnlMenu";
             this.PnlMenu.Size = new System.Drawing.Size(1123, 696);
             this.PnlMenu.TabIndex = 1;
@@ -108,58 +66,63 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Menu";
             // 
-            // pictureBox2
+            // PbxFases
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(215, 93);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(36, 37);
-            this.pictureBox2.TabIndex = 3;
-            this.pictureBox2.TabStop = false;
+            this.PbxFases.Image = ((System.Drawing.Image)(resources.GetObject("PbxFases.Image")));
+            this.PbxFases.Location = new System.Drawing.Point(179, 68);
+            this.PbxFases.Name = "PbxFases";
+            this.PbxFases.Size = new System.Drawing.Size(758, 550);
+            this.PbxFases.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.PbxFases.TabIndex = 2;
+            this.PbxFases.TabStop = false;
             // 
-            // PnlFase2
+            // PbxPersonagem
             // 
-            this.PnlFase2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.PnlFase2.BackColor = System.Drawing.Color.IndianRed;
-            this.PnlFase2.Controls.Add(this.pictureBox3);
-            this.PnlFase2.Controls.Add(this.pictureBox4);
-            this.PnlFase2.Controls.Add(this.label3);
-            this.PnlFase2.Location = new System.Drawing.Point(0, 0);
-            this.PnlFase2.Name = "PnlFase2";
-            this.PnlFase2.Size = new System.Drawing.Size(1123, 696);
-            this.PnlFase2.TabIndex = 4;
+            this.PbxPersonagem.Location = new System.Drawing.Point(212, 86);
+            this.PbxPersonagem.Name = "PbxPersonagem";
+            this.PbxPersonagem.Size = new System.Drawing.Size(32, 33);
+            this.PbxPersonagem.TabIndex = 3;
+            this.PbxPersonagem.TabStop = false;
             // 
-            // pictureBox3
+            // TmrAndar
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(215, 93);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(36, 37);
-            this.pictureBox3.TabIndex = 3;
-            this.pictureBox3.TabStop = false;
+            this.TmrAndar.Tick += new System.EventHandler(this.TmrAndar_Tick);
             // 
-            // pictureBox4
+            // labelX
             // 
-            this.pictureBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(182, 73);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(758, 550);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox4.TabIndex = 2;
-            this.pictureBox4.TabStop = false;
+            this.labelX.AutoSize = true;
+            this.labelX.Location = new System.Drawing.Point(35, 148);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(55, 13);
+            this.labelX.TabIndex = 4;
+            this.labelX.Text = "Posição X";
             // 
-            // label3
+            // labelY
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(3, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(171, 55);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Fase 2";
+            this.labelY.AutoSize = true;
+            this.labelY.Location = new System.Drawing.Point(35, 177);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(55, 13);
+            this.labelY.TabIndex = 4;
+            this.labelY.Text = "Posição Y";
+            // 
+            // LblX
+            // 
+            this.LblX.AutoSize = true;
+            this.LblX.Location = new System.Drawing.Point(75, 148);
+            this.LblX.Name = "LblX";
+            this.LblX.Size = new System.Drawing.Size(14, 13);
+            this.LblX.TabIndex = 4;
+            this.LblX.Text = "X";
+            // 
+            // LblY
+            // 
+            this.LblY.AutoSize = true;
+            this.LblY.Location = new System.Drawing.Point(75, 177);
+            this.LblY.Name = "LblY";
+            this.LblY.Size = new System.Drawing.Size(14, 13);
+            this.LblY.TabIndex = 4;
+            this.LblY.Text = "Y";
             // 
             // FrmJogo
             // 
@@ -168,39 +131,37 @@
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
             this.ClientSize = new System.Drawing.Size(1116, 687);
-            this.Controls.Add(this.PnlFase1);
+            this.Controls.Add(this.LblY);
+            this.Controls.Add(this.LblX);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.PbxPersonagem);
+            this.Controls.Add(this.PbxFases);
             this.Controls.Add(this.PnlMenu);
             this.Name = "FrmJogo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Jogo Matemática 3°ano";
             this.Load += new System.EventHandler(this.FrmJogo_Load);
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.FrmJogo_KeyPress);
-            this.PnlFase1.ResumeLayout(false);
-            this.PnlFase1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.PnlMenu.ResumeLayout(false);
             this.PnlMenu.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            this.PnlFase2.ResumeLayout(false);
-            this.PnlFase2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PbxFases)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PbxPersonagem)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel PnlFase1;
         private System.Windows.Forms.Panel PnlMenu;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Panel PnlFase2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox PbxFases;
+        private System.Windows.Forms.PictureBox PbxPersonagem;
+        private System.Windows.Forms.Timer TmrAndar;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.Label LblX;
+        private System.Windows.Forms.Label LblY;
     }
 }
 
--- Jogo Matamática 3°ano/Form1.cs
+++ Jogo Matamática 3°ano/Form1.cs
@@ -8,13 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Reflection.Emit;
 
 namespace Jogo_Matamática_3_ano
 {
     public partial class FrmJogo : Form
     {
         #region Variáveis Globais
-        int posLinha = 0, posColuna = 0;
+        int posLinha = 0, posColuna = 0, andar = 22,
+            DebugSwith;
         string controle;
         #endregion
         #region Fase 1
@@ -52,21 +54,100 @@ namespace Jogo_Matamática_3_ano
         {
             InitializeComponent();
         }
-        #region Andar
+        #region Controles
         private void FrmJogo_KeyPress(object sender, KeyPressEventArgs e)
         {
             if(e.KeyChar.ToString().ToLower() == "a" || e.KeyChar.ToString().ToLower() == "d" ||
                e.KeyChar.ToString().ToLower() == "w" || e.KeyChar.ToString().ToLower() == "s")
             {
-
+                TmrAndar.Start();
+                controle = e.KeyChar.ToString().ToLower();
+            }
+            //Ativar e desativar o Debug Mode
+            else if(e.KeyChar.ToString().ToLower() == "y")
+            {
+                if(DebugSwith % 2 == 0)
+                {
+                    //Mostrar posição do personagem
+                    DebugSwith++;
+                    labelX.Visible = true;
+                    labelY.Visible = true;
+                    LblX.Visible = true;
+                    LblY.Visible = true;
+                }
+                else
+                {
+                    //Esconder posição do personagem
+                    DebugSwith++;
+                    labelX.Visible = false;
+                    labelY.Visible = false;
+                    LblX.Visible = false;
+                    LblY.Visible = false;
+                }
             }
         }
         #endregion
+
         #region Load form
         private void FrmJogo_Load(object sender, EventArgs e)
         {
-            PnlFase1.Location = new Point(0, 0);
             PnlMenu.Location = new Point(0, 0);
+            PnlMenu.Visible = false;
+            TmrAndar.Stop();
+            //Esconder posição do personagem
+            labelX.Visible = false;
+            labelY.Visible = false;
+            LblX.Visible = false;
+            LblY.Visible = false;
+        }
+        #endregion
+
+        #region Andar
+        private void TmrAndar_Tick(object sender, EventArgs e)
+        {
+            //Coletar a informação de onde o pesonagem está nas posições X e Y
+            int x = PbxPersonagem.Location.X;
+            int y = PbxPersonagem.Location.Y;
+
+            labelX.Text = x.ToString();
+            labelY.Text = y.ToString();
+
+            if (controle == "a")
+            {
+                if (posColuna == 0) return;
+                if (labirinto[posLinha, posColuna - 1] == "1")
+                {
+                    PbxPersonagem.Location = new Point(x - andar, y);
+                    posColuna--;
+                }
+            }
+            else if (controle == "d")
+            {
+                if (posColuna == 30) return;
+                if (labirinto[posLinha, posColuna + 1] == "1")
+                {
+                    PbxPersonagem.Location = new Point(x + andar, y);
+                    posColuna++;
+                }
+            }
+            else if (controle == "w")
+            {
+                if (posLinha == 0) return;
+                if (labirinto[posLinha - 1, posColuna] == "1")
+                {
+                    PbxPersonagem.Location = new Point(x, y - andar);
+                    posLinha--;
+                }
+            }
+            else if (controle == "s")
+            {
+                if (posLinha == 22) return;
+                if (labirinto[posLinha + 1, posColuna] == "1")
+                {
+                    PbxPersonagem.Location = new Point(x, y + andar);
+                    posLinha++;
+                }
+            }
         }
         #endregion
     }
